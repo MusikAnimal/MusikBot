734b725e7a74f0999453b7625d67af7265129a8f
MusikAnimal <ziemba0123@gmail.com>
2015-04-15 01:28:37 -0400

    Progress

--- core.rb
+++ core.rb
@@ -8,4 +8,9 @@ MediaWiki::Gateway.default_user_agent = 'MusikBot/1.1 (http://en.wikipedia.org/M
 mw = MediaWiki::Gateway.new('http://test.wikipedia.org/w/api.php', ignorewarnings: true)
 Auth.login(mw)
 
-PermClerk.init(mw)
+# FIXME: fetch duration from User:MusikBot/PermClerk/Offset in same API call
+if @mw.get("User:MusikBot/PermClerk/Run").to_s.downcase == "true"
+  PermClerk.init(mw)
+else
+  puts "PermClerk disabled"
+end
--- perm_clerk.rb
+++ perm_clerk.rb
@@ -5,40 +5,62 @@ module PermClerk
   require 'date'
   require 'pry'
 
-  USER = "Example One"
-
-  # GET REQUESTS
-  EDIT_IDENTIFIER = "Automated comment"
-  SPLIT_KEY = "====[[User:"
+  EDIT_THROTTLE = 3
+  PERMISSION = "Rollback"
   SEARCH_DAYS = 30
+  SPLIT_KEY = "====[[User:"
+  PERMISSIONS = [
+    # "Account creator",
+    # "Autopatrolled",
+    # "Confirmed",
+    # "File mover",
+    # "Pending changes reviewer",
+    # "Reviewer",
+    "Rollback"
+    # "Template editor"
+  ]
 
   def self.init(mw)
     @mw = mw
-    requests = mw.get("Wikipedia:Requests for permissions/Rollback", {rvsection: 1})
-    sections = requests.split(SPLIT_KEY)
+    @editThrottle = 0
+    @endTimestamp = nil
+    @pageName = "Wikipedia:Requests for permissions/#{PERMISSION}"
+    @startTimestamp = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+    @usersCount = 0
 
-    sections.each_index do |i|
-      links = []
-      section = sections[i]
-      request = section.match(/{{rfplinks\|1=(.*)}}/)
-      if request
-        userName = request[1]
-        sectionId = i #sections.index{|s| s.include?(userName)}
-        puts "(#{sectionId}) #{userName}"
-
-        links += find_link(userName)
-        if links.length > 0
-          puts "#{links.length} links found for #{userName}"
-          comment(section, sectionId, links)
-        else
-          puts "no links found for #{userName}"
-        end
-      end
-      puts "======"
+    for permission in PERMISSIONS
+      # check if task is set to run for this permission
+      process(permission)
     end
   end
 
-  def self.find_link(userName)
+  def self.editPage(newWikitext)
+    if @editThrottle < 3
+      sleep @editThrottle
+      @editThrottle += 1
+      # attempt to save
+      res = @mw.edit(@pageName, newWikitext, {
+        # assert: 'bot',
+        basetimestamp: @baseTimestamp,
+        # bot: true,
+        contentformat: 'text/x-wiki',
+        section: 1,
+        starttimestamp: @startTimestamp,
+        summary: "Bot clerking, #{@usersCount} user#{'s' if @usersCount > 1} with previously declined requests",
+        text: newWikitext
+      })
+      binding.pry
+
+      # check for edit conflict
+      if editConflict
+        editPage(newWikitext)
+      end
+    else
+      reportError("Throttle hit for edit page operation")
+    end
+  end
+
+  def self.findLink(userName)
     currentDate = Date.today
     targetDate = currentDate - SEARCH_DAYS
     links = []
@@ -47,24 +69,79 @@ module PermClerk
       monthName = Date::MONTHNAMES[monthIndex]
       puts monthName
       page = @mw.get("Wikipedia:Requests for permissions/Denied/#{monthName} #{Date.today.year}")
-      matches = page.scan(/{{Usercheck.*#{userName}.*\/Rollback\]\].*(http:\/\/.*)\s+link\]/)
+      # FIXME: (1) use match instead of scan (2) make sure the date itself is within range
+      matches = page.scan(/{{Usercheck.*#{userName}.*\/#{PERMISSION}\]\].*(http:\/\/.*)\s+link\]/)
       links += matches.flatten if matches
     end
 
     return links
   end
 
-  def self.comment(section, sectionId, links)
-    message = ":[[File:Pictogram voting comment.svg|15px|link=]]&nbsp;'''"+EDIT_IDENTIFIER+"''' "
+  def self.newSectionWikitext(section, links)
     linksMessage = links.map{|l| "[#{l}]"}.join
-    if !section.include?(EDIT_IDENTIFIER)
-      comment = "#{message} A request for this user was declined (#{linksMessage}) within the past #{SEARCH_DAYS} days. ~~~~"
-      newWikitext = SPLIT_KEY + section + comment
-      puts comment
-      # save to page
-      puts "saving..."
+    comment = "\n:{{comment|Automated comment}} This user has had #{links.length} request#{'s' if links.length > 1} for #{PERMISSION.downcase} declined in the past #{SEARCH_DAYS} days (#{linksMessage}). ~~~~\n"
+    return SPLIT_KEY + section.gsub(/\n+$/,"") + comment
+  end
+
+  def self.process(permission)
+    newWikitext = []
+
+    setPageProps
+
+    sections = @oldWikitext.split(SPLIT_KEY)
+
+    binding.pry
+
+    sections.each do |section|
+      links = []
+      if userNameMatch = section.match(/{{(?:template\:)?rfplinks\|1=(.*)}}/i)
+        userName = userNameMatch.captures[0]
+        puts section
+
+        if section.match(/{{(?:template\:)?(done|not done|already done)}}/i) || section.match(/:{{comment|Automated comment}}.*MusikBot/)
+          puts "#{userName}'s request already responded to or MusikBot has already commented"
+          newWikitext << SPLIT_KEY + section
+        else
+          puts "Searching for #{userName}"
+
+          links += findLink(userName)
+          if links.length > 0
+            puts "#{links.length} links found for #{userName}"
+            newWikitext << newSectionWikitext(section, links)
+            @usersCount += 1
+          else
+            puts "no links found for #{userName}"
+            newWikitext << SPLIT_KEY + section
+          end
+        end
+        puts "======"
+      else
+        newWikitext << section
+      end
+    end
+
+    @editThrottle = 0
+    editPage(CGI.unescapeHTML(newWikitext.join))
+  end
+
+  # FIXME: move to core.rb
+  def self.reportError(error)
+    abort("Fatal error: #{error}")
+  end
+
+  def self.setPageProps
+    if @editThrottle < 3
+      sleep @editThrottle
+      @editThrottle += 1
+      begin
+        pageObj = @mw.custom_query(prop: 'info|revisions', titles: @pageName, rvprop: 'timestamp|content')[0][0]
+        @endTimestamp = pageObj.attributes['touched']
+        @oldWikitext = pageObj.elements['revisions'][0][0].to_s
+      rescue
+        setPageProps
+      end
     else
-      puts "Already commented"
+      reportError("Unable to fetch page properties")
     end
   end
 end
