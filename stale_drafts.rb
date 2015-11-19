4b77f77d529fb8f2d3d09726e778d7bc6a5c5284
MusikAnimal <ziemba0123@gmail.com>
2015-11-18 23:37:18 -0500

    StaleDrafts: refactoring

--- stale_drafts.rb
+++ stale_drafts.rb
@@ -1,36 +1,15 @@
 $LOAD_PATH << '.'
 require 'mysql2'
 require 'mediawiki-gateway'
-require 'repl'
-require 'httparty'
-require 'auth.rb'
+require 'musikbot'
 require 'date'
 require 'pry-byebug'
 
 MediaWiki::Gateway.default_user_agent = 'MusikBot/1.1 (https://en.wikipedia.org/wiki/User:MusikBot/)'
 
-class Object
-  def present?
-    !blank?
-  end
-
-  def blank?
-    respond_to?(:empty?) ? empty? : !self
-  end
-end
-
 module StaleDrafts
   def self.run
-    @mw = MediaWiki::Gateway.new('https://en.wikipedia.org/w/api.php', bot: true)
-    Auth.login(@mw)
-
-    @env = eval(File.open('env').read)
-
-    exit 1 unless get_page('User:MusikBot/StaleDrafts/Run') == 'true' || @env == :test
-
-    un, pw, host, db, port = Auth.db_credentials(@env)
-    @repl_client = Repl::Session.new(un, pw, host, db, port)
-    @getter = HTTParty
+    @mb = MusikBot::Session.new(inspect)
 
     process_nonredirects
     # process_redirects
@@ -67,12 +46,14 @@ module StaleDrafts
     end
 
     content = content.chomp("|-\n") + "|}\n\n" # "\n{{/Redirects}}"
-    edit_page('User:MusikBot/StaleDrafts/Report', content, "Reporting #{pages.length} stale non-AfC drafts")
+    @mb.edit('User:MusikBot/StaleDrafts/Report',
+      content: content,
+      summary: "Reporting #{pages.length} stale non-AfC drafts"
+    )
   end
 
-  def self.get_api_data(title, throttle = 0)
-    sleep throttle * 5
-    @mw.custom_query(
+  def self.get_api_data(title)
+    @mb.gateway.custom_query(
       titles: 'Draft:' + title,
       prop: 'linkshere|categories|revisions',
       lhprop: 'pageid',
@@ -80,9 +61,6 @@ module StaleDrafts
       lhlimit: 50,
       rvlimit: 50
     ).elements['pages'][0].elements rescue nil
-  rescue MediaWiki::APIError => e
-    raise e and return false if throttle > 5
-    get_api_data(title, throttle + 1)
   end
 
   def self.process_redirects
@@ -90,7 +68,7 @@ module StaleDrafts
     inner_content = ''
 
     pages.each do |page|
-      api_data = @mw.custom_query(
+      api_data = @mb.gateway.custom_query(
         titles: 'Draft:' + page['page_title'],
         prop: 'linkshere',
         lhprop: 'pageid',
@@ -111,9 +89,12 @@ module StaleDrafts
 
     content = "== Redirects ==\n#{pages.length} redirects with 0 backlinks since #{format_date(end_date)}\n\n" \
       "{| class='wikitable sortable'\n! Page\n! Links\n! Last edit\n|-\n" + inner_content
-
     content = content.chomp("|-\n") + "|}\n\n"
-    edit_page('User:MusikBot/StaleDrafts/Report/Redirects', content, "Reporting #{pages.length} stale non-AfC drafts")
+
+    @mb.edit('User:MusikBot/StaleDrafts/Report/Redirects',
+      content: content,
+      summary: "Reporting #{pages.length} stale non-AfC drafts"
+    )
   end
 
   # Repl-related
@@ -122,29 +103,7 @@ module StaleDrafts
       "JOIN enwiki_p.revision WHERE page_namespace = 118 AND page_is_redirect = #{redirects ? 1 : 0} " \
       "AND rev_id = page_latest AND rev_timestamp < '#{end_date}';"
 
-    @repl_client.query(query).to_a
-  end
-
-  # API-related
-  def self.get_page(page, throttle = 0)
-    sleep throttle * 5
-    @mw.get(page)
-  rescue MediaWiki::APIError => e
-    raise e and return false if throttle > 5
-    get_page(page, throttle + 1)
-  end
-
-  def self.edit_page(page, content, summary, throttle = 0)
-    sleep throttle * 5
-    opts = {
-      contentformat: 'text/x-wiki',
-      summary: summary
-    }
-    @mw.edit(page, CGI.unescapeHTML(content), opts)
-    return true
-  rescue MediaWiki::APIError => e
-    raise e and return false if throttle > 5
-    edit_page(page, content, summary, throttle + 1)
+    @mb.repl_client.query(query).to_a
   end
 
   # Utility
