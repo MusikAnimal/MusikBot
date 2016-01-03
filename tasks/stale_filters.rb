692adc0824c0e98626227b537162652b86c61d70
MusikAnimal <ziemba0123@gmail.com>
2016-01-02 22:11:52 -0500

    StaleFilters: i18n ready

--- tasks/stale_filters.rb
+++ tasks/stale_filters.rb
@@ -15,8 +15,7 @@ module StaleFilters
   end
 
   def self.generate_report
-    # FIXME: also report when the last modification was to the filter
-    extended_content = "<div style='font-size:24px'>#{t('title', date: I18n.l(@mb.today, format: :heading))}</div>\n" +
+    extended_content = "<div style='font-size:24px'>#{t(:title, date: I18n.l(@mb.today, format: :heading))}</div>\n" +
       t(:summary, num: stale_filters.length, days: offset) +
       "\n\n{| class='wikitable sortable'\n! " +
       [
@@ -34,18 +33,18 @@ module StaleFilters
 
       extended_content += '| ' + [
         "[[#{t('Special:AbuseFilter')}/#{filter['af_id']}|#{filter['af_id']}]]",
-        filter['af_public_comments'],
+        filter['af_public_comments'].force_encoding('utf-8'),
         t(:plain_link, url: log(filter['af_id']), name: fdate(filter['afl_timestamp'])),
-        t(:user_link, filter['af_user_text']),
+        t(:user_link, name: filter['af_user_text'].force_encoding('utf-8')),
         "[[#{history(filter['af_id'])}|#{fdate(filter['af_timestamp'])}]]",
-        filter['af_hidden'] == 1 ? 'yes' : 'no',
-        filter['af_actions']
+        filter['af_hidden'] == 1 ? t('yes') : t('no'),
+        translate(filter['af_actions'])
       ].join("\n| ") + "\n|-\n"
     end
 
     extended_content = extended_content.chomp("|-\n") + "|}\n\n"
 
-    summary = t(:edit_summary, stale_filters.length)
+    summary = t(:edit_summary, num: stale_filters.length)
 
     @mb.edit(@report_page,
       content: extended_content,
@@ -64,8 +63,8 @@ module StaleFilters
     return @stale_filters if @stale_filters
 
     query = 'SELECT af_id, af_user_text, afl.afl_timestamp, af_timestamp, af_public_comments, af_hidden, af_actions ' \
-      'FROM enwiki_p.abuse_filter af INNER JOIN (SELECT afl_id, afl_filter, MAX(afl_timestamp) afl_timestamp ' \
-      'FROM enwiki_p.abuse_filter_log GROUP BY afl_filter) afl ON afl.afl_filter = af_id ' \
+      "FROM #{@mb.lang}wiki_p.abuse_filter af INNER JOIN (SELECT afl_id, afl_filter, MAX(afl_timestamp) afl_timestamp " \
+      "FROM #{@mb.lang}wiki_p.abuse_filter_log GROUP BY afl_filter) afl ON afl.afl_filter = af_id " \
       "WHERE af_enabled = 1 AND af_deleted = 0 AND afl.afl_timestamp < #{offset_date};"
 
     @stale_filters = @mb.repl_client.query(query).to_a
@@ -76,6 +75,10 @@ module StaleFilters
   end
 
   # Helpers
+  def self.translate(str)
+    str.split(',').map(&:t).join(', ')
+  end
+
   def self.offset_date
     @mb.db_date(@mb.today - offset)
   end
@@ -93,7 +96,7 @@ module StaleFilters
   end
 
   def self.log(id)
-    "//#{@mb.opts['project']}.org/w/index.php?title=#{t('Special:AbuseLog')}&wpSearchFilter=#{id}"
+    "//#{@mb.opts[:project]}.org/w/index.php?title=#{t('Special:AbuseLog')}&wpSearchFilter=#{id}"
   end
 end
 
