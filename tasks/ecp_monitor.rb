40d4d631d6396f034862699e5b4cc8b59666b99a
MusikAnimal <ziemba0123@gmail.com>
2016-07-25 23:06:25 -0400

    ECPMonitor: track create protection

--- tasks/ecp_monitor.rb
+++ tasks/ecp_monitor.rb
@@ -22,9 +22,12 @@ module ECPMonitor
     )
 
     changes = ecp_changes
+    titles = ecp_titles
+    titles.map { |title| title['type'] = 'create' }
 
-    if changes.any? { |page| @mb.parse_date(page['log_timestamp']) > last_run }
-      generate_report(changes)
+    if changes.any? # { |page| @mb.parse_date(page['log_timestamp']) > last_run }
+      changes = group_changes(changes)
+      generate_report(changes + titles)
       run_file = @mb.local_storage('ECPMonitor_lastrun', 'r+')
       run_file.write(@mb.now.to_s)
       run_file.close
@@ -39,13 +42,18 @@ module ECPMonitor
   end
 
   def self.ecp_changes
-    offset_date = (@mb.now - offset).strftime('%Y%m%d%H%M%S')
-    query('SELECT log_timestamp, log_user_text, log_namespace, log_title, log_comment, pr_expiry FROM logging ' \
-      'INNER JOIN page_restrictions WHERE log_page = pr_page ' \
-      "AND (log_action = 'protect' OR log_action = 'modify') AND pr_type = 'edit' " \
+    query('SELECT log_namespace AS namespace, log_title AS title, log_timestamp AS timestamp, pr_expiry AS expiry, ' \
+      'pr_type AS type, log_comment AS summary, log_user_text AS admin FROM logging ' \
+      'INNER JOIN page_restrictions ON log_page = pr_page ' \
+      "WHERE (log_action = 'protect' OR log_action = 'modify') " \
       "AND pr_level = 'extendedconfirmed' AND log_timestamp > '#{offset_date}' " \
-      'GROUP BY log_title ' \
       'ORDER BY log_timestamp DESC').to_a
+  end
+
+  def self.ecp_titles
+    query('SELECT pt_namespace AS namesapce, pt_title AS title, pt_timestamp AS timestamp, pt_expiry AS expiry, ' \
+      'pt_reason AS summary, user_name AS admin FROM protected_titles ' \
+      "INNER JOIN user ON pt_user = user_id WHERE pt_create_perm = 'extendedconfirmed'").to_a
   end
 
   def self.ecp_total
@@ -54,34 +62,65 @@ module ECPMonitor
     ).to_a[0]['count']
   end
 
-  def self.generate_report(pages)
+  def self.group_changes(changes)
+    grouped_changes = []
+
+    changes.each do |change|
+      # different type protected on the same page at the same time (would have to be by same admin)
+      existing_all = grouped_changes.find_index do |gc|
+        gc['title'] == change['title'] && gc['timestamp'] == change['timestamp'] && gc['namespace'] == change['namespace']
+      end
+
+      # same page, protection and type, but older timestamp
+      existing_page_admin = grouped_changes.find_index do |gc|
+        gc['title'] == change['title'] && gc['type'].include?(change['type']) && gc['namespace'] == change['namespace']
+      end
+
+      if existing_all
+        grouped_changes[existing_all]['type'] += ',' + change['type']
+      elsif !existing_page_admin
+        grouped_changes << change
+      end
+    end
+
+    grouped_changes
+  end
+
+  def self.generate_report(changes)
     protect_pages_link = 'https://en.wikipedia.org/w/index.php?title=Special:ProtectedPages&type=edit&level=extendedconfirmed'
 
+    # changes includes protected titles, so re-sort by log timestamp
+    changes.sort! do |a, b|
+      b['timestamp'] <=> a['timestamp']
+    end
+
     markup = "{| class='wikitable sortable' style='position:relative'" \
-      "\n!colspan='5' | Pages recently put under [[WP:30/500|extended confirmed protection]] " \
-        "(#{pages.length} out of [#{protect_pages_link} {{User:MusikBot/ECPMonitor/Total}} total]) <sup>({{purge}})</sup>" \
+      "\n!colspan='6' | Pages recently put under [[WP:30/500|extended confirmed protection]] " \
+        "(#{changes.length} out of [#{protect_pages_link} {{User:MusikBot/ECPMonitor/Total}} total]) <sup>({{purge}})</sup>" \
         "<div style='position:absolute; left:5px; top:3px; font-size:9px'>{{plainlink|url=//en.wikipedia.org/w/" \
         'index.php?title=User:MusikBot/ECPMonitor/Report&action=watch|name=WATCH}}</div>' \
       "\n|-" \
       "\n!Page" \
       "\n!Protected" \
       "\n!Expiry" \
+      "\n!Type" \
       "\n!Summary" \
       "\n!Admin"
 
-    pages.each do |page|
-      page_title = "#{namespace(page['log_namespace'])}#{page['log_title'].tr('_', ' ')}"
+    changes.each do |change|
+      page_title = "#{namespace(change['namespace'])}#{change['title'].tr('_', ' ')}"
       markup += "\n|-" \
         "\n|[[#{page_title}]]" \
-        "\n|style='white-space:nowrap' |#{parse_date(page['log_timestamp'])}" \
-        "\n|style='white-space:nowrap' |#{parse_date(page['pr_expiry'])}" \
-        "\n|style='max-width:400px' |#{page['log_comment']}" \
-        "\n|{{noping|#{page['log_user_text']}}}"
+        "\n|style='white-space:nowrap' |#{parse_date(change['timestamp'])}" \
+        "\n|style='white-space:nowrap' |#{parse_date(change['expiry'])}" \
+        "\n|#{change['type']}" \
+        "\n|style='max-width:400px' |#{change['summary']}" \
+        "\n|{{noping|#{change['admin']}}}"
     end
 
     markup += "\n|}"
 
-    summary = "Reporting #{pages.length} pages recently put under [[WP:30/500|extended confirmed protection]] " \
+    summary = "Reporting #{changes.length} pages recently put under [[WP:30/500|extended confirmed protection]] " \
       "(#{ecp_total} total pages)"
     opts = {
       summary: summary,
@@ -97,6 +136,10 @@ module ECPMonitor
 
   def self.offset
     @offset ||= @mb.get(OFFSET_PAGE).to_i
+  end
+
+  def self.offset_date
+    (@mb.now - offset).strftime('%Y%m%d%H%M%S')
   end
 
   def self.query(sql)
