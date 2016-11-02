54dbfc7a53fa8a4b2527a3487d18c8692513268f
MusikAnimal <ziemba0123@gmail.com>
2016-11-02 19:03:39 -0400

    WishlistSurvey: Use on-wiki config

--- tasks/wishlist_survey.rb
+++ tasks/wishlist_survey.rb
@@ -8,12 +8,11 @@ require 'httparty'
 # --edition 3 instructs to use the 3 set of credentials, in this case for Community_Tech_bot
 # --nobot is necessary unless the account has the bot flag
 module WishlistSurvey
-  SURVEY_ROOT = '2016 Community Wishlist Survey/Sandbox'
-  CATEGORY_ROOT = "#{SURVEY_ROOT}/Categories"
-
   def self.run
-    # pass in true to ignore on-wiki run page: User:Community_Tech_bot/WishlistSurvey/Run
-    @mb = MusikBot::Session.new(inspect, true)
+    @mb = MusikBot::Session.new(inspect)
+
+    @survey_root = @mb.config[:survey_root]
+    @category_root = "#{@survey_root}/Categories"
 
     total_proposals = 0
     all_editors = []
@@ -22,24 +21,36 @@ module WishlistSurvey
       editors = get_editors(category)
       total_proposals += proposals = num_proposals(category)
       all_editors += editors
-      @mb.edit("#{CATEGORY_ROOT}/#{category}/Proposals", content: proposals)
-      @mb.edit("#{CATEGORY_ROOT}/#{category}/Editors", content: editors.length)
+      @mb.edit("#{@category_root}/#{category}/Proposals",
+        content: proposals,
+        summary: "Updating proposal count"
+      )
+      @mb.edit("#{@category_root}/#{category}/Editors",
+        content: editors.length,
+        summary: "Updating editor count"
+      )
     end
 
-    @mb.edit("#{SURVEY_ROOT}/Total proposals", content: total_proposals)
-    @mb.edit("#{SURVEY_ROOT}/Total editors", content: all_editors.uniq.length)
+    @mb.edit("#{@survey_root}/Total proposals",
+      content: total_proposals,
+      summary: "Updating total proposal count"
+    )
+    @mb.edit("#{@survey_root}/Total editors",
+      content: all_editors.uniq.length,
+      summary: "Updating total editor count"
+    )
   end
 
-  # get direct child subpages of CATEGORY_ROOT (and not the /Count pages, etc.)
+  # get direct child subpages of @category_root (and not the /Count pages, etc.)
   def self.categories
     api_root = @mb.gateway.wiki_url
 
     # mediawiki-gateway framework does not support anything outside action=query, so use HTTParty.
     # Opensearch has a weird response, what we want is always the second element in the returned array.
-    # profile=strict ensures we get results that precisely match CATEGORY_ROOT.
+    # profile=strict ensures we get results that precisely match @category_root.
     @categories ||= # cache in instance variable
-      HTTParty.get("#{api_root}?action=opensearch&search=#{CATEGORY_ROOT}/&profile=strict&redirects=resolve&limit=100")[1]
-        .select { |page| !page.sub(CATEGORY_ROOT + '/', '').include?('/') }
+      HTTParty.get("#{api_root}?action=opensearch&search=#{@category_root}/&profile=strict&redirects=resolve&limit=100")[1]
+        .select { |page| !page.sub(@category_root + '/', '').include?('/') }
   end
 
   # get usernames of editors to given category page
