cf8dd54a611c86bd83b776570e41682440d2a029
MusikAnimal <musikanimal@gmail.com>
2017-05-23 13:21:49 -0400

    NPPReport: new task to get stats on new page patrolling

---
+++ tasks/npp_report.rb
@@ -0,0 +1,130 @@
+$LOAD_PATH << '..'
+require 'musikbot'
+
+module NPPReport
+  def self.run
+    @mb = MusikBot::Session.new(inspect, true)
+    @disk_cache = @mb.local_storage || {}
+
+    # Only look for edits made by users who are known to have done reviews
+    reviewers = @disk_cache['reviewers'].uniq
+    reviewers_sql = reviewers.map{ |r| "\"#{r}\"" }.join(',')
+
+    pages = pages_to_check
+
+    pages_with_reviewers = {}
+    reviewed_and_unreviewed = 0
+
+    num_pages = pages.length
+
+    pages.each_with_index do |page, i|
+      iteration_str = "#{i} of #{num_pages}"
+
+      page_id = page['ptrp_page_id']
+      page_is_unreviewed = page['ptrp_reviewed'].to_i == 0
+
+      # get timestamp of first review of the page
+      sql = %{
+        SELECT MIN(log_timestamp) AS min_timestamp,
+               MAX(log_timestamp) AS max_timestamp
+        FROM logging_logindex
+        WHERE log_page = #{page_id}
+        AND (
+          (
+            log_type = 'patrol'
+            AND log_params NOT LIKE '%::auto";i:1%'
+          ) OR (
+            log_type = 'pagetriage-curation'
+            AND log_action = 'reviewed'
+          )
+        );
+      }
+      reviewed_at = @mb.repl.query(sql).to_a.first
+      page_has_been_reviewed = reviewed_at['min_timestamp'].present?
+
+      rev_timestamp_clause = ''
+
+      if page_has_been_reviewed && page_is_unreviewed
+        # check if there was only one reviewer, who evidently reviewed it and unreviewed it
+        sql = %{
+          SELECT COUNT(DISTINCT(log_user_text)) AS count
+          FROM logging_logindex
+          WHERE log_page = #{page_id}
+          AND (
+            (
+              log_type = 'patrol'
+              AND log_params NOT LIKE '%::auto";i:1%'
+            ) OR (
+              log_type = 'pagetriage-curation'
+              AND log_action = 'reviewed'
+            )
+          );
+        }
+        num_reviewers = @mb.repl.query(sql).to_a.first['count']
+
+        if num_reviewers == 1
+          reviewed_and_unreviewed += 1
+          puts "#{iteration_str} ~ -> #{page_id}: reviewer reviewed and unreviewed"
+        else
+          # More than one reviewer but the page is currently unreviewed.
+          # This is likely because it used to be a redirect and is now an article,
+          # so check for number of edits since the last review
+          puts "#{iteration_str} ~ -> #{page_id} last reviewed at: #{reviewed_at['max_timestamp']}:"
+          rev_timestamp_clause = "AND rev_timestamp > #{reviewed_at['max_timestamp']}"
+        end
+      elsif page_has_been_reviewed
+        # Page has been reviewed, so only look at edits before the review took place
+        puts "#{iteration_str} ~ -> #{page_id} first reviewed at: #{reviewed_at['max_timestamp']}:"
+        rev_timestamp_clause = "AND rev_timestamp < #{reviewed_at['min_timestamp']}"
+      end
+
+      # Get revisions for the page leading up to when it was reviewed (if ever)
+      sql = %{
+        SELECT DISTINCT(rev_user_text) AS reviewer
+        FROM revision_userindex
+        WHERE rev_user_text IN (#{reviewers_sql})
+        AND rev_page = #{page_id}
+        #{rev_timestamp_clause}
+      }
+      reviewers = @mb.repl.query(sql).to_a.collect { |r| r['reviewer'] }
+
+      # There were edits by more than one reviewer. If the page is currently reviewed,
+      # we're only accounting for edits that took place before the final review/
+      # OR there were edits by a single reviewer, and the page is still unreviewed.
+      if reviewers.length > 1 || (reviewers.length == 1 && page_is_unreviewed)
+        page_title = get_page_name(page_id)
+        pages_with_reviewers[page_title] = reviewers.length
+        puts "#{iteration_str} ~ #{page_title} = #{reviewers.join(', ')} (#{reviewers.length})"
+      end
+    end
+
+    @disk_cache['pages'] = pages_with_reviewers
+    @disk_cache['pages_reviewed_and_unreviewed'] = reviewed_and_unreviewed
+
+    @mb.local_storage(@disk_cache)
+  end
+
+  def self.get_page_name(page_id)
+    sql = %{
+      SELECT page_title
+      FROM page
+      WHERE page_id = #{page_id}
+      AND page_namespace = 0
+    }
+    @mb.repl.query(sql).to_a.first['page_title']
+  end
+
+  def self.pages_to_check
+    sql = %{
+      SELECT ptrp_page_id, ptrp_reviewed
+      FROM pagetriage_page
+      JOIN page ON page_id = ptrp_page_id
+      WHERE page_is_redirect = 0
+      AND page_namespace = 0
+      AND ptrp_created BETWEEN 20170401000000 AND 20170501000000
+    }
+    @mb.repl.query(sql).to_a
+  end
+end
+
+NPPReport.run
