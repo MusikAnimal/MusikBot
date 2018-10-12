797712fb96649c278b62c4b87a756f214565a509
MusikAnimal <musikanimal@gmail.com>
2018-10-11 22:38:17 -0400

    GeonoticeSync: validate JSON and generate report if there are errors

--- tasks/geonotice_sync.rb
+++ tasks/geonotice_sync.rb
@@ -1,5 +1,6 @@
 $LOAD_PATH << '..'
 require 'musikbot'
+require 'countries'
 
 module GeonoticeSync
   def self.run
@@ -22,43 +23,118 @@ module GeonoticeSync
     return if synced_at > @mb.parse_date(@current_edit.attributes['timestamp'])
 
     begin
-      sync
-      generate_report
+      success = sync
     rescue => e
-      @errors << [
+      @errors << {
         group: I18n.t('errors.fatal'),
         message: I18n.t('errors.fatal_msg',
           log_page: "#{t('User')}:MusikBot II/GeonoticeSync/Error log"
         )
-      ]
+      }
       generate_report
       raise e
     end
 
-    @mb.local_storage(
-      'last_revid' => @current_edit.attributes['revid'],
-      'synced' => @mb.now.to_s
-    )
+    if success
+      @mb.local_storage(
+        'last_revid' => @current_edit.attributes['revid'],
+        'synced' => @mb.now.to_s
+      )
+    end
+
+    generate_report
   rescue => e
     @mb.report_error(t('Fatal error'), e)
+  end
+
+  def self.validate(json)
+    valid = true
+
+    json.each do |title, config|
+      # Keys
+      if (config.keys - %w(begin end country text corners)).any?
+        add_error(title, I18n.t('geonotice.errors.keys'))
+        valid = false
+      end
+      if config['country'].present? && config['corners'].present?
+        add_error(title, I18n.t('geonotice.errors.coords_country'))
+        valid = false
+      end
+
+      # Text
+      if config['text'].blank?
+        add_error(title, I18n.t('geonotice.errors.text'))
+        valid = false
+      end
+
+      # Dates
+      %w(begin end).each do |field|
+        if (Date.parse(config['begin']) rescue nil).nil?
+          add_error(title, I18n.t('geonotice.errors.date', field: "<code>#{field}</code>"))
+          valid = false
+        end
+      end
+
+      # Country
+      if config['country'].present? && ISO3166::Country.new(config['country']).nil?
+        add_error(title, I18n.t('geonotice.errors.country', country: config['country']))
+        valid = false
+      end
+
+      # Corners
+      if config['corners'].present? && validate_corners(title, config['corners']) == false
+        valid = false
+      end
+    end
+
+    valid
+  end
+
+  def self.validate_corners(title, corners)
+    if corners.length != 2
+      add_error(title, I18n.t('geonotice.errors.corners.length', field: '<code>corners</code>'))
+      return false
+    else
+      corners.each do |corner|
+        if corner.length != 2
+          add_error(title, I18n.t('geonotice.errors.corners.coord_length', field: '<code>corners</code>'))
+          return false
+        end
+
+        if !corner[0].is_a?(Float) || !corner[1].is_a?(Float)
+          add_error(title, I18n.t('geonotice.errors.corners.numeric', field: '<code>corners</code>'))
+          return false
+        end
+      end
+    end
+
+    true
+  end
+
+  def self.add_error(title, msg, type = 'syntax')
+    @errors << {
+      group: I18n.t("geonotice.errors.#{type}"),
+      message: "(#{title}) " + msg
+    }
   end
 
   def self.sync
     json = JSON.parse(@mb.get(@config_page))
 
-    # TODO: validate format...
+    return false unless validate(json)
 
-    content = "window.GeoNotice = {};\nwindow.GeoNotice.notices = " + JSON.pretty_generate(json) + ';'
+    comment = @mb.get(@geonotice_page).scan(/(\/\*.*?\*\/)/m).flatten[0]
+    content = "#{comment}\n\nwindow.GeoNotice = {};\nwindow.GeoNotice.notices = " + JSON.pretty_generate(json) + ';'
 
     @mb.edit(@geonotice_page,
       content: content,
       summary: I18n.t('geonotice.sync_summary',
-        link: "[[Special:Diff/#{@mb.local_storage['last_revid']}/#{@current_edit.attributes['revid']}]]"
+        link: "[[Special:Diff/#{@mb.local_storage['last_revid']}/#{@current_edit.attributes['revid']}|#{@config_page}]]"
       )
     )
 
     true
-  rescue JSON::ParserError => e
+  rescue JSON::ParserError
     @errors << {
       group: I18n.t('geonotice.parser').upcase,
       message: I18n.t('geonotice.errors.parser', config_page: @config_page)
@@ -71,20 +147,22 @@ module GeonoticeSync
     content = ''
 
     if @errors.any?
-      content = "<div style='color:red;font-weight:bold'>#{num_errors} error#{'s' if num_errors > 1} as of ~~~~~</div>\n"
+      content = "<div style='color:red;font-weight:bold'>#{I18n.t('geonotice.errors.num_errors', num: @errors.length)}</div>\n"
 
       @errors.each do |error|
         content += "* <span style='font-weight:bold'>#{error[:group]}</span>: #{error[:message]}\n"
       end
+    else
+      content = "<span style='color:green; font-weight:bold'>#{I18n.t('geonotice.errors.no_errors')}</span>\n"
     end
 
-    content += '\n' + I18n.t('geonotice.last_synced',
-      date: @mb.wiki_date(@mb.local_storage['last_sync'])
+    content += "\n" + I18n.t('geonotice.last_synced',
+      date: @mb.wiki_date(@mb.local_storage['synced'])
     )
 
     @mb.edit(@report_page,
       content: content,
-      summary: I18n.t('errors.reporting_errors', num: @errors.length)
+      summary: I18n.t('updating_report', link: "[[#{t('User')}:MusikBot II/GeonoticeSync|GeonoticeSync]]")
     )
   end
 end
