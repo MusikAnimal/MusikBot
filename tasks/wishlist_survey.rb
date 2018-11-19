68cec1463ad4936a02b23f48b345883c53fad728
MusikAnimal <musikanimal@gmail.com>
2018-11-19 10:53:00 -0500

    WishlistSurvey: write voting report to Meta tracking page

--- tasks/wishlist_survey.rb
+++ tasks/wishlist_survey.rb
@@ -406,30 +406,29 @@ module WishlistSurvey
 
     content += "\n\n[[Category:#{@survey_root}]]"
 
-    gateway ||= MediaWiki::Gateway.new('https://www.mediawiki.org/w/api.php',
-      bot: true,
-      retry_count: 5,
-      user_agent: 'User:Community_Tech_bot/1.1 (https://www.mediawiki.org/wiki/User:Community_Tech_bot/)',
-      ignorewarnings: true
-    )
-    credentials = YAML.load(
-      File.open(
-        File.dirname(__FILE__) + '/../config/application.yml'
-      ).read
-    ).symbolize_keys
-    gateway.login(
-      credentials[:api][:edition_3][:username],
-      credentials[:api][:edition_3][:password]
-    )
-
-    gateway.edit('User:Community_Tech_bot/Survey_tracking', content,
-      summary: 'Survey tracking - to be moved to Meta after a few days of voting'
-    )
-
-    # @mb.edit("#{@survey_root}/Tracking",
-    #   content: content,
-    #   summary: "Updating voting results (#{rows.length} proposals, #{@total_editors} editors, #{total_supports} support votes)"
+    # gateway ||= MediaWiki::Gateway.new('https://www.mediawiki.org/w/api.php',
+    #   bot: true,
+    #   retry_count: 5,
+    #   user_agent: 'User:Community_Tech_bot/1.1 (https://www.mediawiki.org/wiki/User:Community_Tech_bot/)',
+    #   ignorewarnings: true
     # )
+    # credentials = YAML.load(
+    #   File.open(
+    #     File.dirname(__FILE__) + '/../config/application.yml'
+    #   ).read
+    # ).symbolize_keys
+    # gateway.login(
+    #   credentials[:api][:edition_3][:username],
+    #   credentials[:api][:edition_3][:password]
+    # )
+    # gateway.edit('User:Community_Tech_bot/Survey_tracking', content,
+    #   summary: 'Survey tracking - to be moved to Meta after a few days of voting'
+    # )
+
+    @mb.edit("#{@survey_root}/Tracking",
+      content: content,
+      summary: "Updating voting results (#{rows.length} proposals, #{@total_editors} editors, #{total_supports} support votes)"
+    )
   end
 
   # Get page ID for given page title, necessary to query revision table.
