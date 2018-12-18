7ba4b23e0963ae7c37485571ec2393aab12bdb13
MusikAnimal <musikanimal@gmail.com>
2018-12-18 11:43:35 -0500

    TFATagger: make template configurable; better edit summary

--- tasks/tfa_tagger.rb
+++ tasks/tfa_tagger.rb
@@ -8,23 +8,25 @@ module TFATagger
     # The Toolforge grid engine may delay the bot till some time after midnight.
     # As a workaround, it is also ran before midnight. This code makes sure it's actually
     # at or past midnight so that we know we're looking at the right TFAs.
-    return unless @mb.now.hour.zero?
+    return unless @mb.now.hour.zero? or @mb.env == 'test'
 
     today = @mb.now.strftime('%B %-d, %Y')
     yesterday = (@mb.now - 1).strftime('%B %-d, %Y')
 
+    template = @mb.get('User:MusikBot/TFATagger/Template')
+
     # Add to current TFA.
     current_tfa = @mb.get("Template:TFA title/#{today}")
     @mb.edit(current_tfa,
-      content: @mb.get(current_tfa).chomp + "\n{{Current TFA}}",
-      summary: 'Tagging as {{Current TFA}}'
+      content: @mb.get(current_tfa).chomp + "\n{{#{template}}}",
+      summary: "Tagging as {{[[:Template:#{template}|#{template}]]}}"
     )
 
     # Remove from previous TFA.
     prev_tfa = @mb.get("Template:TFA title/#{yesterday}")
     @mb.edit(prev_tfa,
-      content: @mb.get(prev_tfa).sub(/\n{{Current TFA}}/, ''),
-      summary: 'Removing {{Current TFA}}'
+      content: @mb.get(prev_tfa).sub(/\n{{#{template}}}/, ''),
+      summary: "Removing {{[[:Template:#{template}|#{template}]]}}"
     )
   end
 end
