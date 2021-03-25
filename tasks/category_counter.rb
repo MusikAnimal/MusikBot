8ffb7861b1698fedb20052b85a67a3b331c007ff
MusikAnimal <musikanimal@gmail.com>
2021-03-24 21:10:59 -0400

    CategoryCounter: finishing touches; working task

--- tasks/category_counter.rb
+++ tasks/category_counter.rb
@@ -9,30 +9,35 @@ module CategoryCounter
     # First determine which datasets need updating based on the granularity.
     categories = @mb.config
     to_update = []
-    categories.each do |entry|
+    categories.each_with_index do |entry, i|
       if should_refresh_data?(entry)
+        # Normalize category title
+        entry[:category] = 'Category:' + entry[:category].gsub(/^Category:/, '')
+
         to_update << entry
       end
     end
 
     date = @mb.now.strftime('%Y-%m-%d')
     sizes = fetch_category_sizes(to_update)
-    to_update.each do |entry|
-      category_title = 'Category:' + entry['category'].gsub(/^Category:/, '')
-      data = JSON.parse(@mb.get(entry['dataset']))
 
-      if entry['cutoff'].present?
-        data = trim_old_data(data, entry['cutoff'])
+    to_update.each do |entry|
+      next unless entry[:dataset] == 'Template:Orphaned articles chart/data'
+      data = JSON.parse(@mb.get(entry[:dataset]))
+
+      if entry[:cutoff].present?
+        data = trim_old_data(data, entry[:cutoff])
       end
 
       data << {
         'date' => date,
-        'value' => sizes[category_title]
+        'value' => sizes[entry[:category]]
       }
 
-      @mb.edit(entry['dataset'],
-        content: data,
-        summary: "Adding size of [[#{category_title}]] as of #{date} ([[User:MusikBot/ALSChart|more info]])"
+      binding.pry
+      @mb.edit(entry[:dataset],
+        content: data.to_json,
+        summary: "Adding size of [[:#{entry[:category]}]] as of #{date} ([[User:MusikBot/CategoryCounter|more info]])"
       )
     end
   rescue => e
@@ -41,7 +46,7 @@ module CategoryCounter
 
   # Remove all data points dated before the present day - 'cutoff' days
   def self.trim_old_data(data, cutoff)
-    data.map do |datum|
+    data.select do |datum|
       @mb.parse_date(datum['date']) > (@mb.today - cutoff.to_i)
     end
   end
@@ -52,17 +57,17 @@ module CategoryCounter
 
     # Batched API requests to 500 per request.
     categories.each_slice(500).to_a.each do |chunk|
-      titles = chunk.map { |c| c['category'] }
+      titles = chunk.map { |c| c[:category] }
 
       pages = @mb.gateway.custom_query(
-        titles: titles,
+        titles: titles.join('|'),
         prop: 'categoryinfo',
         formatversion: 2
       ).elements['pages'].to_a
 
       pages.each do |page|
-        if page['categoryinfo']
-          sizes[page['title']] = page['categoryinfo']['size']
+        if page.elements['categoryinfo']
+          sizes[page['title']] = page.elements['categoryinfo']['size'].to_i
         end
       end
     end
@@ -72,11 +77,11 @@ module CategoryCounter
 
   def self.should_refresh_data?(entry)
     ret = entry[:category].present? && entry[:granularity].present? && entry[:dataset].present?
-    ret && case granularity
+    ret && case entry[:granularity].to_sym
       when :daily
         true
       when :weekly
-        @mb.now.wday == 0
+        @mb.now.wday == 4
       when :monthly
         @mb.now.mday == 1
     end
