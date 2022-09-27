93e2e8a9e8440da37f27c0e32e0362b7b326aebf
MusikAnimal <musikanimal@gmail.com>
2022-09-27 10:01:18 -0400

    TopArticleReviewers: add redir stats per https://w.wiki/5kYT

--- tasks/top_article_reviewers.rb
+++ tasks/top_article_reviewers.rb
@@ -11,7 +11,11 @@ module TopArticleReviewers
       {{TOC right}}
       <div style='font-size:24px'>Top new article reviewers as of ~~~~~</div>
 
-      Only reviews of mainspace pages are counted. Limited to the top 100 reviewers for each time period.
+      Please note the following:
+      * Only reviews of mainspace pages are counted.
+      * The report is limited to the top 100 reviewers for each time period.
+      * The '''Totals''' row refers to totals across all users, not just the top 100 shown in this report.
+      * Due to technical restrictions, '''Redirects''' refer to the ''current'' redirect status of the page, not the status of when the review was made.
 
       Prepared by ~~~ <onlyinclude>~~~~~</onlyinclude>
 
@@ -49,62 +53,160 @@ module TopArticleReviewers
       {| class='wikitable sortable'
       ! Rank
       ! Username
-      ! Num reviews
+      ! Articles
+      ! Redirects
+      ! Total
       ! Log
       |-
     END
 
-    top_reviewers(offset).each_with_index do |data, i|
+    # Keep track of subtotals
+    subtotals = {
+      reviewers: 0,
+      articles: 0,
+      redirects: 0,
+      total: 0
+    }
+
+    get_top_reviewers(offset).each_with_index do |data, i|
       patrol_log_link = "https://en.wikipedia.org/w/index.php?title=Special:Log" \
         "&type=patrol&subtype=patrol&user=#{data['reviewer'].score}"
       pagetriage_log_link = "https://en.wikipedia.org/w/index.php?title=Special:Log" \
         "&type=pagetriage-curation&user=#{data['reviewer'].score}"
 
+      subtotals[:reviewers] += 1
+      subtotals[:total] += data['total']
+      subtotals[:articles] += data['articles']
+      subtotals[:redirects] += data['redirects']
+
       markup += <<~END
         | #{i + 1}
         | {{User0|#{data['reviewer']}}}
-        | data-sort-value=#{data['reviewer']} | {{FORMATNUM:#{data['reviews']}}}
+        | {{FORMATNUM:#{data['articles']}}}
+        | {{FORMATNUM:#{data['redirects']}}}
+        | {{FORMATNUM:#{data['total']}}}
         | [#{patrol_log_link} Patrol] [#{pagetriage_log_link} Page Curation]
         |-
       END
     end
 
-    markup.chomp("\n|-") + "|}\n"
+    # Add subtotals
+    markup += <<~END
+      ! Subtotals
+      ! style="text-align:left" | {{FORMATNUM:#{subtotals[:reviewers]}}} reviewers
+      ! style="text-align:left" | {{FORMATNUM:#{subtotals[:articles]}}}
+      ! style="text-align:left" | {{FORMATNUM:#{subtotals[:redirects]}}}
+      ! style="text-align:left" | {{FORMATNUM:#{subtotals[:total]}}}
+      !
+      |-
+    END
+
+    unless subtotals[:reviewers] < 100
+      # Add remainders
+      markup += <<~END
+        ! Remaining
+        ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['reviewers'] - subtotals[:reviewers]}}} reviewers
+        ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['articles'] - subtotals[:articles]}}}
+        ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['redirects'] - subtotals[:redirects]}}}
+        ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['total'] - subtotals[:total]}}}
+        !
+        |-
+      END
+    end
+
+    # Add totals
+    markup += <<~END
+      ! Totals
+      ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['reviewers']}}} reviewers
+      ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['articles']}}}
+      ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['redirects']}}}
+      ! style="text-align:left" | {{FORMATNUM:#{@totals[offset]['total']}}}
+      !
+    END
+
+    markup + "|}\n"
   end
 
   # Get the top reviewers in the past N days (specified by offset)
-  def self.top_reviewers(offset = 1)
+  def self.get_top_reviewers(offset = 1)
     # Cache so this can be re-called without repeating the query
     return @top_reviewers[offset] if @top_reviewers && @top_reviewers[offset]
 
-    start_time = (@mb.now - offset).strftime('%Y%m%d%H%M%S')
-    end_time = @mb.now.strftime('%Y%m%d%H%M%S')
+    start_time = get_start_time(offset)
+    end_time = get_end_time(offset)
 
+    # First fetch and cache the totals
+    @totals ||= {}
+    @totals[offset] = fetch_totals(start_time, end_time)
+
+    # Cache and return the list of top users
+    @top_reviewers ||= {}
+    @top_reviewers[offset] = fetch_top_reviewers(start_time, end_time)
+  end
+
+  # Run the actual query, summing the number of reviews that are redirects.
+  def self.fetch_top_reviewers(start_time, end_time)
     sql = %{
       SELECT actor_name AS `reviewer`,
-      COUNT(DISTINCT(logtemp.log_page)) AS `reviews`
+        COUNT(DISTINCT(logtemp.log_page)) AS `total`,
+        COUNT(IF(logtemp.page_is_redirect = 1, 1, NULL)) as `redirects`,
+        COUNT(IF(logtemp.page_is_redirect = 0, 1, NULL)) as `articles`
       FROM (
-        SELECT log_actor, log_page
+        SELECT log_actor, log_page, page_is_redirect
         FROM logging_userindex
-        WHERE log_timestamp BETWEEN #{start_time} AND #{end_time}
+        JOIN page ON page_title = log_title AND page_namespace = log_namespace
+        WHERE log_timestamp BETWEEN ? AND ?
         AND log_type = 'patrol'
         AND log_action = 'patrol'
         AND log_namespace = 0
         UNION
-        SELECT log_actor, log_page
+        SELECT log_actor, log_page, page_is_redirect
         FROM logging_userindex
-        WHERE log_timestamp BETWEEN #{start_time} AND #{end_time}
+        JOIN page ON page_title = log_title AND page_namespace = log_namespace
+        WHERE log_timestamp BETWEEN ? AND ?
         AND log_type = 'pagetriage-curation'
         AND log_action = 'reviewed'
         AND log_namespace = 0
       ) logtemp
       JOIN actor ON actor_id = log_actor
       GROUP BY reviewer
-      ORDER BY reviews DESC
+      ORDER BY articles DESC
       LIMIT 100;
     }
-    @top_reviewers ||= {}
-    @top_reviewers[offset] = @mb.repl.query(sql).to_a
+    @mb.repl_query(sql, start_time, end_time, start_time, end_time).to_a
+  end
+
+  def self.fetch_totals(start_time, end_time)
+    sql = %{
+      SELECT COUNT(DISTINCT log_actor) AS `reviewers`,
+        COUNT(*) AS `total`,
+        COUNT(IF(logtemp.page_is_redirect = 1, 1, NULL)) as `redirects`,
+        COUNT(IF(logtemp.page_is_redirect = 0, 1, NULL)) as `articles`
+      FROM (
+        SELECT DISTINCT log_actor, log_page, page_is_redirect
+        FROM logging_userindex
+          JOIN page ON page_title = log_title AND page_namespace = log_namespace
+        WHERE log_timestamp BETWEEN ? AND ?
+          AND 
+          ((log_type = 'patrol'  AND log_action = 'patrol') OR
+           (log_type = 'pagetriage-curation' AND log_action = 'reviewed'))
+          AND log_namespace = 0
+      ) logtemp
+    }
+    @mb.repl_query(sql, start_time, end_time).to_a.first
+  end
+
+  def self.get_start_time(offset)
+    # Return cached start time if it exists, to ensure all queries have the exact same date
+    return @start_time[offset] if @start_time && @start_time[offset]
+    @start_time ||= {}
+    @start_time[offset] = (@mb.now - offset).strftime('%Y%m%d%H%M%S')
+  end
+
+  def self.get_end_time(offset)
+    return @end_time[offset] if @end_time && @end_time[offset]
+    @end_time ||= {}
+    @end_time[offset] = @mb.now.strftime('%Y%m%d%H%M%S')
   end
 end
 
